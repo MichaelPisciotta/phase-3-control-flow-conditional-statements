81062de76eea0d6038b27f9439424e870161c15e
MichaelPisciotta <mfp56@miami.edu>
2021-09-20 17:53:54 -0400

    Done.

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,56 @@
 def admin_login(username, password)
-  # your code here
+  if ((username == "admin" || username === "ADMIN") && password === "12345")
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
+ 
+  
+  if temperature < 40
+     "brisk"
+  elsif temperature > 40 && temperature < 65
+     "a little chilly"
+  elsif temperature > 85 
+     "too dang hot"
+  else 
+     "perfect"
+   
+     "It\'s #{temperature} out there!"
 end
 
-def fizzbuzz(num)
-  # your code here
-end
 
-def calculator(operation, num1, num2)
-  # your code here
-end
+
+
+
+
+# def fizzbuzz(num)
+#   # your code here
+# end
+
+
+# def fizzbuzz(num) 
+#   if (num % 3 === 0 && num % 5 === 0) {
+#     return "FizzBuzz";
+#   } else if (num % 3 === 0) {
+#     return "Fizz";
+#   } else if (num % 5 === 0) {
+#     return "Buzz";
+#   } else {
+#     return num;
+#   }
+
+
+
+
+
+
+
+
+# def calculator(operation, num1, num2)
+#   # your code here
+# end
 
